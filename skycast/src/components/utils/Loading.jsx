de68b5ca6667c2dcd034378b23339465f2aba354
Gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-15 11:10:39 -0300

    Custom Loading component.
    Utilizing LucideDev for the icons. Implementing useEffect and useState to set an interval for displaying the icons, as well as managing the text color and dot animation. Adjusting the speed of the spinner.

--- skycast/src/components/utils/Loading.jsx
+++ skycast/src/components/utils/Loading.jsx
@@ -1,26 +1,55 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { CloudLightning, CloudRain, Cloud, Sun } from "lucide-react";
+import { useState, useEffect } from "react";
 
 function Loading () {
 
     const icons = [
         {
             id: "thuderstorm",
-            icon: <CloudLightning size={48} color="#ffcc00"/>
+            icon: <CloudLightning size={38} color="#ffcc00"/>
         },
         {
             id: "cloudy-sun",
-            icon: <Sun size={48} color="#f1c40f"/>
+            icon: <Sun size={38} color="#f1c40f"/>
         },
         {
             id: "rainy",
-            icon: <CloudRain size={48} color="#3498db"/>
+            icon: <CloudRain size={38} color="#3498db"/>
         },
         {
             id: "cloudy",
-            icon: <Cloud size={48} color="#95a5a6"/>
+            icon: <Cloud size={38} color="#95a5a6"/>
         }
     ]
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [dotCount, setDotCount] = useState(1);
+    const [isBlue, setIsBlue] = useState(false);
+
+    // Changing the icon in every 1.2 seconds
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % icons.length);
+        }, 1200);
+        
+            return () => clearInterval(interval);
+        }, []);
+
+    // Changing the color of and thethe text and the dots in every 0.5 seconds
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDotCount((prevDotCount) => (prevDotCount % 3) + 1);
+            setIsBlue((prevIsBlue) => !prevIsBlue);
+        }, 500);
+        
+            return () => clearInterval(interval);
+        }, []);
+
+        const dots = '.'.repeat(dotCount);
+        const textColor = isBlue ? "text-blue-500" : "text-yellow-500";
+
 
     return (
         <motion.div
@@ -29,35 +58,33 @@ function Loading () {
         transition={{duration: 0.6}}
         className="flex justify-center items-center h-40 w-40 relative">
 
-            {icons.map((icon, idx) => (
-            
+            {/* Spinner */}
+            <motion.div
+            animate={{rotate: 360}}
+            transition={{duration: 2, repeat: Infinity, ease: "linear"}}
+            className="absolute rounded-full border-4 border-dashed border-blue-500 h-20 w-20"/>
+
+            {/* Animated icon */}
+
+            <AnimatePresence mode="wait">
+
                 <motion.div
-                key={icon.id}
-                initial={{opacity: 0}}
-                animate={{opacity: [0, 1,0]}}
-
-                transition= {{
-                duration: 4, 
-                repeat: Infinity,
-                repeatDelay: icons.length, 
-                ease: "easeInOut", 
-                delay: idx
-                }}
-
+                key={icons[currentIndex].id}
+                initial={{opacity: 0, scale:0.5}}
+                animate={{opacity: 1, scale:1.2}}
+                exit={{opacity: 0, scale:0.5}}
+                transition={{duration: 0.4, ease: "easeOut"}}
                 className="absolute">
 
-                    {icon.icon}
+                    {icons[currentIndex].icon}
 
                 </motion.div>
+            </AnimatePresence>
 
-            ))}
+            <div className={`mt-40 text-lg font-bold transition-colors duration-300 ${textColor}`}>
+                Loading{dots}
+            </div>
 
-            {/* Spinner */}
-            <motion.div 
-            animate={{rotate: 360}}
-            transition={{duration: 1, repeat: Infinity, ease: "linear"}}
-            className="absolute rounded-full border-4 border-dashed border-gray-300 h-20 w-20"/>
-               
         </motion.div>
     )
 
