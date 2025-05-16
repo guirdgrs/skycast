ad784261fb4f806d8093f5cc988b34ad34070512
gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-16 16:47:29 -0300

    Changing the icons of the API to the Lucide icons.

--- skycast/src/components/forecast/ForecastList.jsx
+++ skycast/src/components/forecast/ForecastList.jsx
@@ -1,6 +1,20 @@
 import { AnimatePresence, hover, motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import { hoverAnimation } from "../utils/motionConfig";
+import { hoverAnimation, entryAnimation } from "../utils/motionConfig";
+
+import  {
+    Sun,
+    Cloud,
+    CloudRain,
+    CloudSnow,
+    Zap,
+    Wind,
+    Droplet,
+    CloudDrizzle,
+    CloudFog,
+    CloudSun,
+    CloudMoon
+} from "lucide-react";
 
 function ForecastList ({data}) {
 
@@ -16,11 +30,29 @@ function ForecastList ({data}) {
         }
     }
 
+    // Mapping the weather icons
+    const weatherIconsMap = {
+        Clear: Sun,
+        Clouds: Cloud,
+        Rain: CloudRain,
+        Drizzle: CloudDrizzle,
+        Snow: CloudSnow,
+        Thunderstorm: Zap,
+        Mist: CloudFog,
+        Smoke: CloudFog,
+        Haze: CloudFog,
+        Dust: CloudFog,
+        Fog: CloudFog,
+        Sand: CloudFog,
+        Ash: CloudFog,
+        Squall: Wind,
+        Tornado: Wind
+    }
+
     return (
         <AnimatePresence>
         <motion.div
-        initial={{opacity: 0, y: 20}}
-        animate={{opacity: 1, y: 0}}
+        {...entryAnimation}
         className="space-y-2">
 
             <h3 className="text-lg font-semibold text-center">Upcoming Forecast</h3>
@@ -30,6 +62,10 @@ function ForecastList ({data}) {
             <div className="w-full overflow-x-auto hide-scrollbar">
                 <div className="flex justify-center gap-4 pb-2 mt-6 w-max mx-auto">
                     {daily.map((item, idx) => {
+                        // Destructuring the data
+                        const weatherMain = item.weather[0].main;
+                        // Mapping the weather icons
+                        const WeatherIcon = weatherIconsMap[weatherMain] || Sun;
                         // Formatting the date * 1000 is to convert from seconds to milliseconds
                         const date = new Date(item.dt * 1000);
                         // Receiving the weekday and storing it as long
@@ -53,7 +89,7 @@ function ForecastList ({data}) {
                         <Link
                         to={`/forecast/${item.dt}`}
                         key={idx}
-                        className="min-w-[120px] dark:bg-blue-700 rounded-xl p-3 shadow-yellow-600 shadow-md cursor-pointer
+                        className="group min-w-[120px] dark:bg-blue-700 rounded-xl p-3 shadow-yellow-600 shadow-md cursor-pointer
                         hover:bg-yellow-500 hover:shadow-blue-600
                         hover:border-2 hover:border-blue-600 
                         transition-discrete transition-colors">
@@ -68,13 +104,10 @@ function ForecastList ({data}) {
                                 </p>
                                 
                                 <hr className="mt-2 rounded-2xl"/> */}
-                                
-                                <img 
-                                src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} 
-                                alt="weather icon" 
-                                className="w-10 h-10 mx-auto"/>
 
-                                <p className="text-center text-white text-sm">
+                                <WeatherIcon className="w-10 h-10 mx-auto text-yellow-400 group-hover:text-blue-600" />
+
+                                <p className="text-center text-white text-sm mt-2 group-hover:text-blue-600">
                                     {Math.round(item.main.temp)}°C
                                 </p>
                             </motion.div>
