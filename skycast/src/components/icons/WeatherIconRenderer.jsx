7d27adf0daa830606f7a7268852bc2c0e51ebfd2
Gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-23 21:31:17 -0300

    Update the IconRenderer.
    Updating the IconRenderer component with the new icon styling component.

--- skycast/src/components/icons/WeatherIconRenderer.jsx
+++ skycast/src/components/icons/WeatherIconRenderer.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import weatherIconsMap from "../utils/weatherIcons.js";
+import { weatherIconStyles } from "../utils/weatherIconsStyle.js";
 
 // Function to render weather icons
 // Receives the condition and optional size and className
 function WeatherIconRenderer({ condition, size = 40, className = "" }) {
   const IconComponent = weatherIconsMap[condition];
+  // Get the style class based on the condition
+  const styleClass = weatherIconStyles[condition] || "text-gray-500";
+
+  // If no icon is found, display a message
+  if(!IconComponent) {
+    return <span className="text-xs text-gray-400">No icon</span>
+  }
 
   // Render the weather icon
-  return IconComponent ? (
-    // React.createElement is used to create an instance of the weather icon
-    React.createElement(IconComponent, {
-      size,
-      className,
-    })
-  ) : (
-    // If no icon is found, display a message
-    <span className="text-xs text-gray-400">No icon</span>
+  return (
+    // The classname is a combination of the style class and the optional className
+    <IconComponent 
+    size={size} 
+    className={`${styleClass} ${className}`}/>
   );
 }
 
