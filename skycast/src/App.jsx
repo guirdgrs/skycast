2c59613eb30265450605586cfabf12996cfb3d85
Gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-24 20:16:21 -0300

    Testing the error component.

--- skycast/src/App.jsx
+++ skycast/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FavIconManager from './components/utils/FavIconManager'
 import ForecastDetails from './components/forecast/ForecastDetails';
+import ErrorTestPage from './components/pages/ErrorTestPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/forecast/:timestamp' element={<ForecastDetails/>}/>
+        <Route path='/error' element={<ErrorTestPage/>}/>
       </Routes>
     </Router>
   )
---
+++ skycast/src/components/pages/ErrorTestPage.jsx
@@ -0,0 +1,11 @@
+import Error from "../utils/Error";
+
+function ErrorTestPage () {
+    return (
+        <div>
+            <Error title="Error" message="This is an error message" />
+        </div>
+    )
+}
+
+export default ErrorTestPage;
