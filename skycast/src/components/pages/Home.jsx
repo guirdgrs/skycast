8bdc5079f3b48caa0e1d8e85e01bce22de80cd5c
Gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-24 20:49:09 -0300

    Aplying the error component on the home.
    Plus fixing the redundant code issue.

--- skycast/src/components/pages/Home.jsx
+++ skycast/src/components/pages/Home.jsx
@@ -5,6 +5,7 @@ import Loading from "../utils/Loading";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import useWeatherData from "../hooks/useWeatherData";
+import Error from "../utils/Error";
 
 function App (){
 
@@ -34,8 +35,10 @@ function App (){
 
         console.log("API forecast raw:", res.data);
         setForecast(res.data.list || []);
-      } catch (error) {
-        console.error("An error occurred while fetching forecast:", error);
+      } catch {
+        <Error
+        title="Unable to fetch forecast"
+        message="An error occurred while fetching the forecast. Please try again later." />
       }
     }
 
@@ -43,30 +46,16 @@ function App (){
   }, [weatherData]);
 
     if (loading) return <div className="flex items-center justify-center"><Loading /></div>
-    if (error) return <p>An error has occurred</p>
+    if (error) return <Error title="Unable to fetch weather" message="An error occurred while fetching the weather. Please try again later."/>
 
 
     return (
-        <div className="max-w-4xl mx-auto p-4 space-y-8 font-sans text-lg">
+      <div className="max-w-4xl mx-auto p-4 space-y-8 font-sans text-lg">
 
-            {loading ? (
-                
-                <div className="flex items-center justify-center z-50">
-                    <Loading />
-                </div>
-                
-            ) : error ? (
-                <p className="text-red-500 text-center">
-                    Unable to access your location. Please allow location access.
-                </p>
-            ) : (
-                <>
-                    {weatherData && <WeatherCard data={weatherData} />}
-                    {forecast.length > 0 && <ForecastList data={forecast} />}
-                </>
-            )}
-
-        </div>
+        {weatherData && <WeatherCard data={weatherData} />}
+        {forecast.length > 0 && <ForecastList data={forecast} />}
+        
+      </div>
     )
 
 }
