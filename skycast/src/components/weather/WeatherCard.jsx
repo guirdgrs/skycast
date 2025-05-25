26f02c07f2d14577f9592ce1ffd632fabc0eecdb
Gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-24 21:18:15 -0300

    Using the modal component.

--- skycast/src/components/weather/WeatherCard.jsx
+++ skycast/src/components/weather/WeatherCard.jsx
@@ -3,6 +3,7 @@ import { MapPin } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 import WeatherIconRenderer from "../icons/WeatherIconRenderer";
 import { fadeSlideAnimation, hoverSmallAnimation, modalAnimation } from "../utils/motionConfig";
+import CityModal from "../modal/CityModal";
 
 // WeatherCard starts by receiving the data from the parent component
 function WeatherCard ({data}) {
@@ -10,32 +11,8 @@ function WeatherCard ({data}) {
     // Destructuring the data into variables
     const {name, weather, main} = data;
 
+    // State for the modal
     const [showModal, setShowModal] = useState(false);
-    const modalRef = useRef();
-
-    useEffect(() => {
-        function handleClickOutside(event) {
-            if(modalRef.current && !modalRef.current.contains(event.target)) {
-                setShowModal(false);
-            }
-        }
-
-        function handleKeyDown(event) {
-            if(event.key === "Escape") {
-                setShowModal(false);
-            }
-        }
-
-        if (showModal) {
-            document.addEventListener("mousedown", handleClickOutside);
-            document.addEventListener("keydown", handleKeyDown);
-        }
-
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-            document.removeEventListener("keydown", handleKeyDown);
-        }
-    })
 
     return (
         <motion.div
@@ -59,36 +36,11 @@ function WeatherCard ({data}) {
             size={64}
             className="hover:text-yellow-500 hover:fill-yellow-400 transition-colors"/>
 
-            <AnimatePresence>
-            {showModal && (
-                <motion.div 
-                className="fixed inset-0 backdrop-blur-lg flex items-center justify-center z-50 font-sans"
-                {...fadeSlideAnimation}>
-                    <motion.div 
-                    ref={modalRef}
-                    className="bg-blue-700 p-6 rounded-xl max-w-md w-full space-y-4 relative shadow-md text-yellow-400 border-2 border-yellow-400 shadow-yellow-400"
-                    {...modalAnimation}>
+            <CityModal
+            isOpen={showModal}
+            onClose={() => setShowModal(false)}
+            cityName={name}/>
 
-                        <button
-                            onClick={() => setShowModal(false)}
-                            className="absolute top-2 right-3 text-yellow-400 hover:text-blue-600 hover:bg-yellow-400 p-1 px-2 rounded cursor-pointer">
-                            ✕
-                        </button>
-
-                    <h3 className="text-lg font-semibold">Current City</h3>
-
-                    <p className="text-xl font-bold">{name}</p>
-
-                    <hr />
-
-                    <input
-                        type="text"
-                        placeholder="Search for another location..."
-                        className="w-full mt-4 p-2 rounded bg-yellow-400 text-blue-600 placeholder-blue-600"/>
-                    </motion.div>
-                </motion.div>
-            )}
-            </AnimatePresence>
         </motion.div>
     )
 }
