bbe174604c1e71b48389f3a6c3a03ddffe4d123b
gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-28 15:43:13 -0300

    Implementing the fuzzy search on the modal.

--- skycast/src/components/modal/CityModal.jsx
+++ skycast/src/components/modal/CityModal.jsx
@@ -1,11 +1,16 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import { fadeSlideAnimation, modalAnimation } from "../utils/motionConfig"
+import axios from "axios";
 
 
-function CityModal({isOpen, onClose, cityName}) {
+function CityModal({isOpen, onClose, cityName, onCitySelect, onUseCurrentLocation}) {
 
     const modalRef = useRef();
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
         function handleClickOutside(event) {
@@ -30,6 +35,37 @@ function CityModal({isOpen, onClose, cityName}) {
             document.removeEventListener("keydown", handleKeyDown);
         };
     }, [isOpen, onClose]);
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(async () => {
+            if(searchTerm.length < 3) {
+                setSuggestions([]);
+                setIsLoading(false);
+                return;
+            }
+
+            setIsLoading(true);
+
+            try {
+                const apiKey = "fad766a91179faefd351fa6b913315e6";
+                const res = await axios.get("https://api.openweathermap.org/geo/1.0/direct", {
+                    params: {
+                        q: searchTerm,
+                        limit: 5,
+                        appid: apiKey
+                    }
+                });
+
+                setSuggestions(res.data);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }, 500);
+
+        return () => clearTimeout(delayDebounceFn);
+    }, [searchTerm]);
 
     return (
         <AnimatePresence>
@@ -58,7 +94,27 @@ function CityModal({isOpen, onClose, cityName}) {
                     <input
                         type="text"
                         placeholder="Search for another location..."
-                        className="w-full mt-4 p-2 rounded bg-yellow-400 text-blue-600 text-sm placeholder-blue-600"/>
+                        className="w-full mt-4 p-2 rounded bg-yellow-400 text-blue-600 text-sm placeholder-blue-600"
+                        value={searchTerm} 
+                        onChange={(e) => setSearchTerm(e.target.value)}/>
+
+                        <ul className="space-y-1">
+                            {suggestions.map((city, index) => (
+                                <li 
+                                    key={index}
+                                    onClick={() => {
+                                        onCitySelect(city);
+                                        setSearchTerm("");
+                                        onClose();
+                                    }}
+                                    className="cursor-pointer text-white hover:bg-yellow-400 hover:text-blue-600 rounded px-2 py-1 transition-colors">
+                                    {city.name}, {city.state ? `${city.state}, ` : ""} {city.country}
+                                </li>
+                            ))}
+                        </ul>
+
+                        {isLoading && <p className="text-white loading">Loading...</p>}
+                         
                     </motion.div>
                 </motion.div>
             )}
