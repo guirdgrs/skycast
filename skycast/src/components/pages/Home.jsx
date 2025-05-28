aca132ca10b4bd8033615b394f90d07dcf049ffa
gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-28 16:21:01 -0300

    Updating the Home.

--- skycast/src/components/pages/Home.jsx
+++ skycast/src/components/pages/Home.jsx
@@ -10,13 +10,11 @@ import useForecastData from "../hooks/useForecastData";
 
 function App (){
 
-    // Receiving the data from the hook
-    const {weatherData, loading: weatherLoading, error: weatherError} = useWeatherData();
+    const [selectedCoords, setSelectedCoords] = useState(null);
+    const { weatherData, loading, error, loading: weatherLoading, error:weatherError, fetchWeatherByCoordinates } = useWeatherData();
+    const { forecast, loading: forecastLoading, error:forecastError } = useForecastData(selectedCoords || weatherData?.coord);
 
-    const { forecast, loading: forecastLoading, error: forecastError } = useForecastData(weatherData);
-
-
-    if (weatherLoading || forecastLoading) {
+if (weatherLoading || forecastLoading) {
       return <div className="flex items-center justify-center"><Loading /></div>
     }
 
@@ -28,11 +26,20 @@ function App (){
       return <Error title="Unable to fetch forecast" message="An error occurred while fetching the forecast. Please try again later."/>
     }
 
+    const handleCitySelect = async (city) => {
+      setSelectedCoords({ lat: city.lat, lon: city.lon });
+      await fetchWeatherByCoordinates(city.lat, city.lon);
+    };
 
     return (
       <div className="max-w-4xl mx-auto p-4 space-y-8 font-sans text-lg">
 
-        {weatherData && <WeatherCard data={weatherData} />}
+        {weatherData && <WeatherCard 
+        weatherData={weatherData} 
+        loading={loading}
+        error={error}
+        onCitySelect={handleCitySelect}/>}
+
         {forecast.length > 0 && <ForecastList data={forecast} />}
 
       </div>
